96e712f089885e12fec1f96aea7722578dd7b1de
Daniel Gerber <daniel.gerber@deinestadtsuchtdich.de>
2013-11-18 15:26:23 +0100

    start scripts

--- defacto-core/startEvidenceEvaluation.sh
+++ defacto-core/startEvidenceEvaluation.sh
@@ -1,3 +1,3 @@
 export MAVEN_OPTS="-Xmx12000m"
 mvn -o compile
-mvn exec:java -Dexec.mainClass="org.aksw.defacto.evaluation.DefactoEvaluation" -Dexec:args="train"
+mvn exec:java -Dexec.mainClass="org.aksw.defacto.evaluation.DefactoEvaluation" -Dexec.args="train"
