5fface0dac7dc45425006e21842bf486fef0a2eb
Daniel Gerber <daniel.gerber@deinestadtsuchtdich.de>
2013-12-07 21:06:45 +0100

    asdf

--- defacto-core/src/main/java/org/aksw/defacto/Constants.java
+++ defacto-core/src/main/java/org/aksw/defacto/Constants.java
@@ -66,7 +66,7 @@ public class Constants {
 				"là", "ma", "maintenant", "mais", "mes", "mine", "moins", "mon", "mot", "même", "ni", "nommés", "notre", "nous", "nouveaux", "ou", "où", "par", "parce",
 				"parole", "pas", "personnes", "peut", "peu", "pièce", "plupart", "pour", "pourquoi", "quand", "que", "quel", "quelle", "quelles", "quels", "qui", "sa", 
 				"sans", "ses", "seulement", "si", "sien", "son", "sont", "sous", "soyez", "sujet", "sur", "ta", "tandis", "tellement", "tels", "tes", "ton", "tous", 
-				"tout", "trop", "très", "tu", "valeur", "voie", "voient", "vont", "votre", "vous", "vu", "ça", "étaient", "état", "étions", "été", "être", "daughter");
+				"tout", "trop", "très", "tu", "valeur", "voie", "voient", "vont", "votre", "vous", "vu", "ça", "étaient", "état", "étions", "été", "être");
 	}
 	
 	public static final Set<String> NEW_STOP_WORDS = new HashSet<String>();
@@ -87,7 +87,7 @@ public class Constants {
 				"parole", "pas", "personnes", "peut", "peu", "pièce", "plupart", "pour", "pourquoi", "quand", "que", "quel", "quelle", "quelles", "quels", "qui", "sa", 
 				"sans", "ses", "seulement", "si", "sien", "son", "sont", "sous", "soyez", "sujet", "sur", "ta", "tandis", "tellement", "tels", "tes", "ton", "tous", 
 				"tout", "trop", "très", "tu", "valeur", "voie", "voient", "vont", "votre", "vous", "vu", "ça", "étaient", "état", "étions", "été", "être",
-				"dean", "priscilla", "bates", "alan", "prinzessin", "bayern", "österreich");
+				"dean", "priscilla", "bates", "alan", "prinzessin", "bayern", "österreich", "daughter");
 	}
 	
 	/**
