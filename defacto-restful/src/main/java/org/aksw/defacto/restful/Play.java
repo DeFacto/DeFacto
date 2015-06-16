4be00bc5da118aef1e636c0418703dbb1cbc7e27
renespeck <speckrene@gmail.com>
2015-06-16 17:53:01 +0200

    Change to new classes.

--- defacto-restful/src/main/java/org/aksw/defacto/restful/Play.java
+++ defacto-restful/src/main/java/org/aksw/defacto/restful/Play.java
@@ -3,30 +3,26 @@ package org.aksw.defacto.restful;
 import java.util.Calendar;
 
 import org.aksw.defacto.Defacto;
+import org.aksw.defacto.Defacto.TIME_DISTRIBUTION_ONLY;
 import org.aksw.defacto.config.DefactoConfig;
 import org.aksw.defacto.evidence.Evidence;
 import org.aksw.defacto.model.DefactoModel;
+import org.aksw.defacto.restful.core.DummyData;
 import org.aksw.defacto.util.AGDISTIS;
 import org.aksw.defacto.util.AGDISTISResult;
-import org.aksw.defacto.util.DummyData;
 import org.aksw.defacto.util.EvidenceRDFGenerator;
 import org.ini4j.Ini;
 
 public class Play {
     public static String test = "";
 
-    public static void main2(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         Defacto.DEFACTO_CONFIG = new DefactoConfig(new Ini(Defacto.class.getClassLoader().getResourceAsStream("defacto.ini")));
-        AGDISTISResult result = AGDISTIS.disambiguate("Albert Einstein", "Nobel Prize");
-        System.out.println(result);
 
         final DefactoModel model = DummyData.getEinsteinModel();
 
-        // this is actually a dummy call of DeFacto
-        // final Pair<DefactoModel, Evidence> evidence =
-        // DummyData.createDummyData(5);//TODO call DeFacto properly
         Calendar startTime = Calendar.getInstance();
-        final Evidence evidence = Defacto.checkFact(model, Defacto.TIME_DISTRIBUTION_ONLY.NO);
+        final Evidence evidence = Defacto.checkFact(model, TIME_DISTRIBUTION_ONLY.NO);
         Calendar endTime = Calendar.getInstance();
 
         System.out.println(evidence.getDeFactoScore());
@@ -35,7 +31,7 @@ public class Play {
         System.out.println(EvidenceRDFGenerator.getProvenanceInformationAsString(DummyData.getDummyTriple(), evidence, startTime, endTime, "TURTLE"));
     }
 
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
 
         try {
             Defacto.DEFACTO_CONFIG = new DefactoConfig(new Ini(Defacto.class.getClassLoader().getResourceAsStream("defacto.ini")));
