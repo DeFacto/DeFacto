0ce090d50779d930864c8d9939224dbd8058dcbf
renespeck <speckrene@gmail.com>
2015-06-16 03:19:09 +0200

    Update backend.

--- defacto-restful/src/main/java/org/aksw/defacto/restful/core/RestModel.java
+++ defacto-restful/src/main/java/org/aksw/defacto/restful/core/RestModel.java
@@ -1,16 +1,31 @@
 package org.aksw.defacto.restful.core;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
 import org.aksw.defacto.Constants;
+import org.aksw.defacto.Defacto;
+import org.aksw.defacto.config.DefactoConfig;
 import org.aksw.defacto.model.DefactoModel;
-import org.apache.jena.riot.Lang;
+import org.aksw.defacto.restful.utils.Cfg;
+import org.aksw.jena_sparql_api.core.QueryExecutionFactory;
+import org.aksw.jena_sparql_api.http.QueryExecutionFactoryHttp;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.dllearner.kb.sparql.SparqlEndpoint;
+import org.ini4j.Ini;
 
 import com.hp.hpl.jena.graph.NodeFactory;
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
@@ -22,32 +37,50 @@ import com.hp.hpl.jena.rdf.model.Resource;
  */
 public class RestModel {
 
-    public DefactoModel getTestModel() {
-        // -----------------------------------------------------------
-        // input
-        String from = "1910";
-        String to = "1930";
+    public static Logger      LOG  = null;
+    public static Set<String> lang = null;
 
-        String objectURI = "http://dbpedia.org/resource/Nobel_Prize_in_Physics";
-        Map<String, String> objectLabels = new HashMap<String, String>();
-        objectLabels.put("en", "Nobel Prize in Physics");
-        objectLabels.put("de", "Nobelpreis für Physik");
-        objectLabels.put("fr", "Prix Nobel de physique");
+    // sets langs
+    static {
+        PropertyConfigurator.configure(Cfg.LOG_FILE);
+        LOG = LogManager.getLogger(RestModel.class);
+        try {
+            Defacto.DEFACTO_CONFIG = new DefactoConfig(new Ini(Defacto.class.getClassLoader().getResourceAsStream("defacto.ini")));
+            lang = new HashSet<>(Arrays.asList(Defacto.DEFACTO_CONFIG.getStringSetting("boa", "languages").split(",")));
+        } catch (IOException e) {
+            LOG.error(e.getLocalizedMessage(), e);
+        }
+        if (lang == null) {
+            lang = new HashSet<>(Arrays.asList(new String[] { "en" }));
+        }
+    }
 
-        String subjectURI = "http://dbpedia.org/resource/Albert_Einstein";
-        String subjectProperty = "http://dbpedia.org/ontology/placeholder";
-        Map<String, String> subjectLabels = new HashMap<String, String>();
-        subjectLabels.put("en", "Albert Einstein");
-        subjectLabels.put("de", "Albert Einstein");
-        subjectLabels.put("fr", "Albert Einstein");
+    private Date              date = new Date();
 
-        String blankURI = subjectURI.concat("__123");
-        String blankProperty = "http://dbpedia.org/ontology/award";
-        // -----------------------------------------------------------
+    /**
+     * 
+     * @param triple
+     * @param from
+     * @param to
+     * @return
+     */
+    public DefactoModel getModel(Triple triple, String from, String to) {
+
+        String subjectURI = triple.getSubject().getURI().toString();
+        String blankProperty = triple.getPredicate().getURI().toString();
+        String objectURI = triple.getObject().getURI().toString();
+
+        // create labels
+        Set<String> set = new HashSet<>();
+        set.add(subjectURI);
+        set.add(blankProperty);
+        set.add(objectURI);
+
+        Model langModel = generateModel(set, lang);
 
         return getDefactoModel(
-                getModel(objectURI, subjectURI, blankURI, objectLabels, subjectLabels, subjectProperty, blankProperty, from, to),
-                objectLabels.keySet());
+                getModel(langModel, objectURI, subjectURI, blankProperty, from, to),
+                lang);
     }
 
     /**
@@ -75,6 +108,25 @@ public class RestModel {
 
     /**
      * 
+     * @param model
+     * @param objectURI
+     * @param subjectURI
+     * @param blankProperty
+     * @param from
+     * @param to
+     * @return
+     */
+    public Model getModel(Model model,
+            String objectURI, String subjectURI,
+            String blankProperty,
+            String from, String to
+            ) {
+        Map<String, String> empty = new HashMap<>();
+        return getModel(model, objectURI, subjectURI, empty, empty, blankProperty, from, to);
+    }
+
+    /**
+     * 
      * 
      * @param objectURI
      * @param subjectURI
@@ -88,15 +140,78 @@ public class RestModel {
      * @return
      */
     public Model getModel(
-            String objectURI, String subjectURI, String blankURI,
+            String objectURI, String subjectURI, Map<String, String> objectLabels, Map<String, String> subjectLabels,
+            String blankProperty, String from, String to
+            ) {
+        return getModel(
+                null, objectURI, subjectURI, objectLabels, subjectLabels,
+                blankProperty, from, to);
+    }
+
+    /**
+     * 
+     * @param uris
+     * @param languages
+     * @return
+     */
+    protected Model generateModel(Set<String> uris, Set<String> langs) {
+        String rdfs_label = "http://www.w3.org/2000/01/rdf-schema#label";
+
+        QueryExecutionFactory qef = new QueryExecutionFactoryHttp(
+                SparqlEndpoint.getEndpointDBpedia().getURL().toString(),
+                SparqlEndpoint.getEndpointDBpedia().getDefaultGraphURIs()
+                );
+
+        Model model = ModelFactory.createDefaultModel();
+        for (String uri : uris) {
+            String query = "CONSTRUCT {<" + uri + "> <" + rdfs_label + "> ?o} WHERE {<" + uri + "> <" + rdfs_label + "> ?o.";
+            if (lang.size() > 0) {
+                query += "FILTER(";
+
+                int i = 0;
+                for (String lang : langs) {
+                    query += "LANGMATCHES(LANG(?o),'" + lang + "')";
+                    if (i++ < langs.size() - 1)
+                        query += " || ";
+                }
+                query += ")";
+            }
+            query += "}";
+
+            QueryExecution qe = qef.createQueryExecution(query);
+            qe.execConstruct(model);
+            qe.close();
+        }
+        return model;
+    }
+
+    /**
+     * 
+     * @param model
+     * @param objectURI
+     * @param subjectURI
+     * @param objectLabels
+     * @param subjectLabels
+     * @param blankProperty
+     * @param from
+     * @param to
+     * @return
+     */
+    protected Model getModel(Model model,
+            String objectURI, String subjectURI,
             Map<String, String> objectLabels, Map<String, String> subjectLabels,
-            String subjectProperty, String blankProperty,
+            String blankProperty,
             String from, String to
             ) {
 
-        // create model
-        Model model = ModelFactory.createDefaultModel();
+        String time = Long.toString(date.getTime());
+        String subjectProperty = "http://dbpedia.org/ontology/tmp".concat("_").concat(time);
+        String blankURI = subjectURI.concat("__").concat(time);
 
+        // create model
+        if (model == null) {
+            model = ModelFactory.createDefaultModel();
+        }
         // object
         Resource objectRes = model.createResource(objectURI);
         for (Entry<String, String> entry : objectLabels.entrySet()) {
@@ -115,8 +230,6 @@ public class RestModel {
             subjectRes.addProperty(Constants.RDFS_LABEL, model.createLiteral(entry.getValue(), entry.getKey()));
         }
         subjectRes.addProperty(model.createProperty(subjectProperty), blank.asResource());
-
-        model.write(System.out, Lang.TURTLE.getName());
 
         return model;
     }
--- defacto-restful/src/main/java/org/aksw/defacto/restful/webservice/Controller.java
+++ defacto-restful/src/main/java/org/aksw/defacto/restful/webservice/Controller.java
@@ -1,16 +1,47 @@
 package org.aksw.defacto.restful.webservice;
 
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletResponse;
 
-import org.aksw.defacto.restful.Play;
+import org.aksw.defacto.Defacto;
+import org.aksw.defacto.boa.Pattern;
+import org.aksw.defacto.config.DefactoConfig;
+import org.aksw.defacto.evidence.ComplexProof;
+import org.aksw.defacto.evidence.Evidence;
+import org.aksw.defacto.evidence.WebSite;
+import org.aksw.defacto.model.DefactoModel;
+import org.aksw.defacto.restful.core.DummyData;
+import org.aksw.defacto.restful.core.RestModel;
 import org.aksw.defacto.restful.mongo.MongoDB;
+import org.aksw.defacto.restful.utils.Cfg;
+import org.aksw.defacto.util.FactBenchExample;
+import org.aksw.defacto.util.FactBenchExamplesLoader;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.ini4j.Ini;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.common.collect.ComparisonChain;
+import com.hp.hpl.jena.graph.NodeFactory;
+import com.hp.hpl.jena.graph.Triple;
 import com.mongodb.DBObject;
 
 /**
@@ -19,28 +50,224 @@ import com.mongodb.DBObject;
  *
  */
 @RestController
-@RequestMapping("/rest")
+@RequestMapping("/fusion")
 public class Controller {
+    static {
+        PropertyConfigurator.configure(Cfg.LOG_FILE);
+    }
+    public static Logger LOG       = LogManager.getLogger(Controller.class);
 
-    protected MongoDB db = new MongoDB();
+    protected MongoDB    db        = null;
+    protected JSONArray  examples  = null;
+    protected RestModel  restModel = null;
 
     /**
      * 
      */
     @PostConstruct
-    public void init() {
+    protected void init() {
+        try {
+            Defacto.DEFACTO_CONFIG = new DefactoConfig(new Ini(Defacto.class.getClassLoader().getResourceAsStream("defacto.ini")));
+        } catch (IOException e) {
+            LOG.error(e.getLocalizedMessage(), e);
+        }
 
+        db = new MongoDB();
+        restModel = new RestModel();
+
+        examples = new JSONArray();
+        for (FactBenchExample example : FactBenchExamplesLoader.loadExamples()) {
+            examples.put(
+                    new JSONObject()
+                            .put("fact", example.getFact())
+                            .put("s", example.getTriple().getSubject().getURI())
+                            .put("p", example.getTriple().getPredicate().getURI())
+                            .put("o", example.getTriple().getObject().getURI())
+                    );
+        }
+    }
+
+    /**
+     * service path: examples/<br>
+     * 
+     * Example facts and triples.
+     * 
+     * @return json array
+     * 
+     */
+    @RequestMapping(
+            value = "/examples",
+            headers = "Accept=application/json",
+            produces = "application/json",
+            method = RequestMethod.GET
+            )
+            public @ResponseBody String examples() {
+        return examples.toString();
+    }
+
+    /**
+     * service path: exampleinput/<br>
+     * 
+     * @return json object
+     */
+    @RequestMapping(
+            value = "/exampleinput",
+            headers = "Accept=application/json",
+            produces = "application/json",
+            method = RequestMethod.GET)
+    @ResponseBody
+    String inputExample(HttpServletResponse response) {
+        Triple t = DummyData.getDummyTriple();
+        return input(new JSONObject()
+                .put("s", t.getSubject().getURI().toString())
+                .put("p", t.getPredicate().getURI().toString())
+                .put("o", t.getObject().getURI().toString())
+                .put("from", "1910")
+                .put("to", "1930")
+                .toString(), response);
     }
 
     /**
      * 
-     * @return
+     * service path: input/<br>
+     * 
+     * @param s
+     * @param p
+     * @param o
+     * @param from
+     * @param to
+     * @return json object
      */
-    @RequestMapping("/")
+    @RequestMapping(
+            value = "/input",
+            headers = "Accept=application/json",
+            produces = "application/json",
+            method = RequestMethod.POST)
     @ResponseBody
-    String home() {
-        Play.main(null);
-        return Play.test;
+    String input(
+            @RequestBody String json, HttpServletResponse response
+            ) {
+
+        JSONObject in = null;
+        try {
+            in = new JSONObject(json);
+            // TODO
+            // checkinput
+        } catch (Exception e) {
+            LOG.error(e.getLocalizedMessage(), e);
+            try {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Wrong input.");
+                response.flushBuffer();
+            } catch (IOException ee) {
+                LOG.error(ee.getLocalizedMessage(), ee);
+            }
+            return new JSONObject().toString();
+        }
+
+        DefactoModel defactoModel = null;
+
+        // fact is set if input is an example
+        if (in.has("fact")) {
+            for (FactBenchExample example : FactBenchExamplesLoader.loadExamples()) {
+                if (example.getFact().equals(in.getString("fact"))) {
+                    defactoModel = example.getModel();
+                    break;
+                }
+            }
+        }
+
+        // user input
+        if (defactoModel == null) {
+            defactoModel = restModel
+                    .getModel(
+                            new Triple(
+                                    NodeFactory.createURI(in.getString("s")),
+                                    NodeFactory.createURI(in.getString("p")),
+                                    NodeFactory.createURI(in.getString("o"))
+                            ),
+                            in.getString("from") == null ? "" : in.getString("from"),
+                            in.getString("to") == null ? "" : in.getString("to")
+                    );
+        }
+        // call of DeFacto
+        //
+        final Calendar startTime = Calendar.getInstance();
+        LOG.info(startTime);
+        final Evidence evidence = Defacto.checkFact(defactoModel, Defacto.TIME_DISTRIBUTION_ONLY.NO);
+        final Calendar endTime = Calendar.getInstance();
+        LOG.info(endTime);
+
+        // download:
+        // TODO
+        //
+
+        // output:
+        response.setStatus(HttpServletResponse.SC_OK);
+        return out(evidence);
+    }
+
+    protected String out(Evidence evidence) {
+        // sort websites bei defacto score
+        List<WebSite> webSites = evidence.getAllWebSites();
+        Collections.sort(webSites, new Comparator<WebSite>() {
+            @Override
+            public int compare(WebSite o1, WebSite o2) {
+                return ComparisonChain.start()
+                        .compare(o2.getScore(), o1.getScore())
+                        .compare(o1.getTitle(), o2.getTitle())
+                        .result();
+            }
+        });
+
+        JSONArray jaWebSites = new JSONArray();
+        for (final WebSite website : webSites) {
+            JSONArray jaProofs = new JSONArray();
+            int cnt = 1;
+            List<Pattern> boaPatterns = evidence.getBoaPatterns();
+            for (ComplexProof proof : evidence.getComplexProofs(website)) {
+
+                for (Pattern pattern : boaPatterns) {
+                    if (!pattern.getNormalized().trim().isEmpty() && proof.getProofPhrase().toLowerCase().contains(pattern.getNormalized().toLowerCase())) {
+                        break;
+                    }
+                }
+                if (!proof.getTinyContext().contains("http:") && !proof.getTinyContext().contains("ftp:")) {
+                    JSONArray jaLabels = new JSONArray();
+                    Set<String> labels = new HashSet<String>();
+                    labels.add(website.getQuery().getSubjectLabel());
+                    labels.add(website.getQuery().getObjectLabel());
+                    for (String label : labels)
+                        jaLabels.put(label);
+
+                    jaProofs.put(new JSONObject()
+                            .put("label", cnt++)
+                            .put("tinyContext", proof.getTinyContext())
+                            .put("keywords", jaLabels)
+                            );
+                } else {
+                    LOG.warn("TINY:" + proof.getTinyContext());
+                }
+            }// all proofs
+
+            jaWebSites.put(new JSONObject()
+                    .put("url", website.getUrl())
+                    .put("title", website.getTitle())
+                    .put("proofs", jaProofs)
+                    .put("coverage", website.getTopicCoverageScore())
+                    .put("search", website.getTopicMajoritySearchFeature())
+                    .put("web", website.getTopicMajorityWebFeature())
+                    .put("score", website.getScore())
+                    );
+
+        } // all weSites
+        return new JSONObject()
+                .put("score", DecimalFormat.getPercentInstance(Locale.ENGLISH).format(evidence.getDeFactoScore()))
+                .put("from", evidence.defactoTimePeriod == null ? "" : evidence.defactoTimePeriod.getFrom())
+                .put("to", evidence.defactoTimePeriod == null ? "" : evidence.defactoTimePeriod.getTo())
+                .put("websitesSize", evidence.getAllWebSites().size())
+                .put("websites", jaWebSites)
+                .toString();
+
     }
 
     // -- MONGO
