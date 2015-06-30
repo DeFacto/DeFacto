bb87de793ecbdedf99b0fbbfee8299761017b2c5
renespeck <speckrene@gmail.com>
2015-06-30 14:18:16 +0200

    Updates frontend.

--- defacto-restful/src/main/resources/public/js/controller_facts.js
+++ defacto-restful/src/main/resources/public/js/controller_facts.js
@@ -1,42 +1,89 @@
 'use strict';
 
-var controllers = angular.module('defacto.controllers');
-controllers.controller('FactsCtrl', function($routeParams, $scope, $http) {
-    $scope.requested=0;
-    var id = $routeParams.id 
-    $scope.facts = { id:id };
+controllers.controller('FactsCtrl', function($location, $routeParams, $scope, $http) {
+  $scope.requested = 0;
+  if ($routeParams.name) {
+    // call backend with given id
+    $scope.requested = 1;
+    $http
+      .get('/fusion/vote/' + $routeParams.name + '/')
+      .success(function(data) {
+
+        $scope.facts = {
+          id: $routeParams.name,
+          fact: []
+        };
+
+        if (data && typeof data === 'object' && data !== null && data.length) {
+          $scope.facts.fact = data;
+        } else {
+          $scope.status = 'No valid data!';
+        }
+      })
+      .error(function(err) {
+        $scope.status = err;
+      });
+  }
 
 
-    $http.get('fusion/exampleinput').success(function(data) {
-            $scope.requested=1;
-            delete $scope.fact;
-            var maxScore=0,maxCoverage=0,maxSearch=0,maxWeb=0;
-                    angular.forEach(data.websites, function(website) {
-                        maxScore = website.score > maxScore?website.score:maxScore;
-                        maxCoverage= website.coverage > maxCoverage?website.coverage:maxCoverage;
-                        maxSearch = website.search > maxSearch?website.search:maxSearch;
-                        maxWeb = website.web > maxWeb?website.web:maxWeb;
-                    });
+  $scope.sendExampleInput = function() {
 
-              
-                    $scope.fact = {
-                            score:data.score,
-                            from:data.from,
-                            to:data.to,
-                            websitesSize:data.websitesSize,
-                            websites: data.websites,
-                            
-                            maxScore : maxScore,
-                            maxCoverage: maxCoverage,
-                            maxSearch :maxSearch,
-                            maxWeb :maxWeb
-                     }; 
-            
-	})
-
+    var postdata = [{
+      s: 'http://dbpedia.org/resource/Albert_Einstein',
+      p: 'http://dbpedia.org/ontology/award',
+      o: 'http://dbpedia.org/resource/Nobel_Prize_in_Physics'
+    }, {
+      s: 'http://dbpedia.org/resource/Albert_Einstein',
+      p: 'http://dbpedia.org/ontology/award',
+      o: 'http://dbpedia.org/resource/AFL_Rising_Star'
+    }, {
+      s: 'http://dbpedia.org/resource/Albert_Einstein',
+      p: 'http://dbpedia.org/ontology/award',
+      o: 'http://dbpedia.org/resource/Academy_Award_for_Best_Original_Song'
+    }, {
+      s: 'http://dbpedia.org/resource/Albert_Einstein',
+      p: 'http://dbpedia.org/ontology/award',
+      o: 'http://dbpedia.org/resource/World_Food_Prize'
+    }];
     /*
-    $http.get('fusion/store/' + id).success(function(data) {
-		$scope.data = data;
-	})
+    var postdata = [{
+      s: 'http://dbpedia.org/resource/Albert_Einstein',
+      p: 'http://dbpedia.org/ontology/award',
+      o: 'http://dbpedia.org/resource/Nobel_Prize_in_Physics'
+    }];
     */
+    $scope.exampleinput = postdata;
+
+    $http
+      .post('/fusion/inputs/', postdata)
+      .success(function(data) {
+        $scope.input = data;
+      })
+      .error(function(err) {
+        $scope.status = err;
+      });
+  };
+
+
+  $scope.downvote = function(fact) {
+    vote(fact, 'down');
+  };
+  $scope.upvote = function(fact) {
+    vote(fact, 'up');
+  };
+  //
+  var vote = function(fact, dir) {
+    $http
+      .post('/fusion/vote/', {
+        id: $scope.facts.id,
+        fact: fact,
+        dir: dir
+      })
+      .success(function(data) {
+        $scope.input = data;
+      })
+      .error(function(err) {
+        $scope.status = err;
+      });
+  };
 });
--- defacto-restful/src/main/resources/public/js/controller_home.js
+++ defacto-restful/src/main/resources/public/js/controller_home.js
@@ -1,75 +1,46 @@
 'use strict';
 
-var controllers = angular.module('defacto.controllers');
+controllers.controller('HomeCtrl', function($location, $scope, $http, $filter) {
 
-controllers.controller('HomeCtrl', function($location, $scope, $http,$filter) {
-      
-      $scope.requested=0;
-      
-    // load examples and page
-    $http.get('fusion/examples').success(function(data) {
-		$scope.examples = data;
+  $scope.requested = 0;
 
-        $scope.searchFacts = [];
-        angular.forEach(data, function(example) {
-             $scope.searchFacts.push({fact:example.fact});
-        });
-        
-        $scope.currentPage = 1;
-        $scope.maxSize = 5;
-        $scope.bigTotalItems = $scope.examples.length;
-        $scope.bigCurrentPage = 1;
-        $scope.pageSize = 10;
-        $scope.filtered = [];
-        
-        $scope.$watch('input', function (input) {
-            $scope.currentPage = 1;
-            $scope.filtered = $filter('filter')( $scope.searchFacts , input);
-            $scope.noOfPages = $scope.filtered.length / $scope.pageSize;
-        });
+  // load examples and page
+  $http.get('fusion/examples').success(function(data) {
+    $scope.examples = data;
+
+    $scope.searchFacts = [];
+    angular.forEach(data, function(example) {
+      $scope.searchFacts.push({
+        fact: example.fact
+      });
     });
 
-    $scope.setPage = function (pageNo) {
-        $scope.currentPage = pageNo;
-    };
+    $scope.currentPage = 1;
+    $scope.maxSize = 5;
+    $scope.bigTotalItems = $scope.examples.length;
+    $scope.bigCurrentPage = 1;
+    $scope.pageSize = 10;
+    $scope.filtered = [];
 
-    //
-    $scope.getFact = function(example){
-                        $scope.requested=1;
-                        delete $scope.fact;
-                        
-                        var body = {
-                                fact:example.fact,
-                                s: example.s,
-                                p: example.p,
-                                o: example.o,
-                                from:'',
-                                to:''            
-                            };
+    $scope.$watch('input', function(input) {
+      $scope.currentPage = 1;
+      $scope.filtered = $filter('filter')($scope.searchFacts, input);
+      $scope.noOfPages = $scope.filtered.length / $scope.pageSize;
+    });
+  });
 
-                        $http.post('fusion/input/',body).success(function(data) {
+  $scope.setPage = function(pageNo) {
+    $scope.currentPage = pageNo;
+  };
 
-                            var maxScore=0,maxCoverage=0,maxSearch=0,maxWeb=0;
-                            angular.forEach(data.websites, function(website) {
-                                maxScore = website.score > maxScore?website.score:maxScore;
-                                maxCoverage= website.coverage > maxCoverage?website.coverage:maxCoverage;
-                                maxSearch = website.search > maxSearch?website.search:maxSearch;
-                                maxWeb = website.web > maxWeb?website.web:maxWeb;
-                            });
+  $scope.getFact = function(example) {
+    $scope.requested = 1;
+    delete $scope.fact;
 
-                      
-                            $scope.fact = {
-                                    score:data.score,
-                                    from:data.from,
-                                    to:data.to,
-                                    websitesSize:data.websitesSize,
-                                    websites: data.websites,
-                                    
-                                    maxScore : maxScore,
-                                    maxCoverage: maxCoverage,
-                                    maxSearch :maxSearch,
-                                    maxWeb :maxWeb
-                             };                                
-                        });
-                    };
+    $http.post('fusion/input/', {
+      fact: example.fact
+    }).success(function(data) {
+      $scope.fact = data;
+    });
+  };
 });
