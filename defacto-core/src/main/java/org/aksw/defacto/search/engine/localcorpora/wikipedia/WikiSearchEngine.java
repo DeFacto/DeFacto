d15712b5cd81c474672cf55f4a3fd8e62a729d48
Diego Esteves <diegoesteves@gmail.com>
2015-09-05 00:28:20 +0200

    local corpora alpha version done / will create a branch before that for master / please do not deploy in production

--- defacto-core/src/main/java/org/aksw/defacto/search/engine/localcorpora/wikipedia/WikiSearchEngine.java
+++ defacto-core/src/main/java/org/aksw/defacto/search/engine/localcorpora/wikipedia/WikiSearchEngine.java
@@ -1,17 +1,15 @@
-package org.aksw.defacto.search.engine.wikipedia;
+package org.aksw.defacto.search.engine.localcorpora.wikipedia;
 
 
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.sparql.core.ResultBinding;
 import com.hp.hpl.jena.sparql.engine.http.QueryEngineHTTP;
-import org.aksw.defacto.Constants;
 import org.aksw.defacto.Defacto;
 import org.aksw.defacto.boa.Pattern;
 import org.aksw.defacto.evidence.WebSite;
-import org.aksw.defacto.search.engine.DefaultSearchEngine;
 
+import org.aksw.defacto.search.engine.SearchEngine;
 import org.aksw.defacto.search.query.MetaQuery;
 
 import org.aksw.defacto.search.query.SolrWikiQuery;
@@ -20,20 +18,13 @@ import org.aksw.defacto.search.result.SearchResult;
 import org.aksw.defacto.util.JsonReader;
 import org.aksw.defacto.wikipedia.WikipediaSearchResult;
 
-import org.apache.log4j.Logger;
+import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.impl.BinaryRequestWriter;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.BinaryRequestWriter;
-import org.apache.solr.client.solrj.impl.HttpSolrServer;
-import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.SolrDocument;
-import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.common.SolrException;
-import org.apache.solr.common.SolrInputDocument;
+import org.slf4j.LoggerFactory;
 
 import java.net.URL;
 import java.util.*;
@@ -41,32 +32,53 @@ import java.util.*;
 /**
  * Created by esteves on 01.09.15.
  */
-public class WikiSearchEngine extends DefaultSearchEngine {
+public class WikiSearchEngine implements SearchEngine {
 
-    private HttpSolrServer server;
-    private String NUMBER_OF_SEARCH_RESULTS;
-    private static Logger logger =  Logger.getLogger(WikiSearchEngine.class);
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(WikiSearchEngine.class);
+
+    private HttpSolrServer server_wiki_en;
+    private HttpSolrServer server_wiki_fr;
+    private HttpSolrServer server_wiki_de;
+
+    private String NUMBER_OF_SEARCH_RESULTS_LC;
     private String inputField;
     private String outputFields;
 
-    public static void main(String[] args) {
-        Defacto.init();
-
-        MetaQuery q = new MetaQuery("Ghostbusters II|-|?D? NONE ?R?|-|Bill Murray|-|en");
-        WikiSearchEngine engine = new WikiSearchEngine();
-        System.out.println(engine.query(q, null).getTotalHitCount());
-    }
-
     public WikiSearchEngine(){
 
-        server = new HttpSolrServer(Defacto.DEFACTO_CONFIG.getStringSetting("local_corpora", "wiki_service_url"));
-        server.setRequestWriter(new BinaryRequestWriter());
+        server_wiki_en = new HttpSolrServer(Defacto.DEFACTO_CONFIG.getStringSetting("local_corpora", "wiki_service_en_url"));
+        server_wiki_en.setRequestWriter(new BinaryRequestWriter());
+
+        server_wiki_fr = new HttpSolrServer(Defacto.DEFACTO_CONFIG.getStringSetting("local_corpora", "wiki_service_fr_url"));
+        server_wiki_fr.setRequestWriter(new BinaryRequestWriter());
+
+        server_wiki_de = new HttpSolrServer(Defacto.DEFACTO_CONFIG.getStringSetting("local_corpora", "wiki_service_de_url"));
+        server_wiki_de.setRequestWriter(new BinaryRequestWriter());
 
         inputField = Defacto.DEFACTO_CONFIG.getStringSetting("local_corpora", "wiki_fields_in");
         outputFields = Defacto.DEFACTO_CONFIG.getStringSetting("local_corpora", "wiki_fields_out");
 
         if ( Defacto.DEFACTO_CONFIG != null ) {
-            NUMBER_OF_SEARCH_RESULTS = Defacto.DEFACTO_CONFIG.getStringSetting("crawl", "NUMBER_OF_SEARCH_RESULTS");
+            NUMBER_OF_SEARCH_RESULTS_LC = Defacto.DEFACTO_CONFIG.getStringSetting("crawl", "NUMBER_OF_SEARCH_RESULTS_LC");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Defacto.init();
+
+        try{
+            WikiSearchEngine engine = new WikiSearchEngine();
+            MetaQuery q = new MetaQuery("Ghostbusters II|-|?D? NONE ?R?|-|Bill Murray|-|en");
+            SearchResult result = engine.getSearchResults(q, null);
+            System.out.println("done!");
+
+            System.out.println("total of websites: " + result.getWebSites().size());
+            System.out.println("total hit count: " + result.getTotalHitCount());
+            System.out.println("is cached: " + result.isCached());
+
+        }catch (Exception e){
+            System.out.println(e.toString());
         }
 
     }
@@ -78,31 +90,53 @@ public class WikiSearchEngine extends DefaultSearchEngine {
     }
 
     @Override
-    public SearchResult query(MetaQuery query, Pattern pattern) {
+    public SearchResult getSearchResults(MetaQuery query, Pattern pattern) {
+        return query(query, pattern);
+    }
+
+    @Override
+    public SearchResult query(MetaQuery query, Pattern pattern){
+
 
         Map<String, WikipediaSearchResult> lc_results = new HashMap<>();
+        List<WebSite> websites = new ArrayList<>();
+        String language = query.getLanguage().toString();
+        String solrGeneratedStringQuery;
+
+        SolrServer server = null;
 
         try {
 
-            String _query = this.generateQuery(query);
-            System.out.println(_query);
+            if (language.equals("en")) {
+                server = server_wiki_en;
+            }else if (language.equals("fr")){
+                server = server_wiki_fr;
+            }else if (language.equals("de")){
+                server = server_wiki_de;
+            }else{
+                throw new Exception("undefined language");
+            }
 
-            if (query.getLanguage().equals("en")) {
+            solrGeneratedStringQuery = generateQuery(query);
+            System.out.println("Solr generated query: " + solrGeneratedStringQuery);
+
+            solrGeneratedStringQuery = "Ghostbusters II";
+
+            //exclude it as I finish the de and fr indexing from wikipedia!
+            if (language.equals("en")) {
 
                 /*****************************************************
                  query indexed wikipedia (_query)
                  *****************************************************/
 
-                HttpSolrServer solr2 = new HttpSolrServer("http://localhost:8123/solr/wiki_" + query.getLanguage());
-
-                SolrQuery solrqry = new SolrQuery("text" + ":\"" + "Ghostbusters II" + "\"").setRows(1000);
+                SolrQuery solrqry = new SolrQuery("text" + ":\"" + solrGeneratedStringQuery + "\"").setRows(Integer.parseInt(NUMBER_OF_SEARCH_RESULTS_LC));
                 solrqry.setFields("id", "titleText");
-                QueryResponse response = solr2.query(solrqry);
+                QueryResponse response = server.query(solrqry);
 
                 int iaux = 1;
                 String siteurl = "";
                 String ids = "";
-                List<WebSite> results = new ArrayList<WebSite>();
+
 
                 for (SolrDocument doc : response.getResults()) {
                     System.out.println(doc.toString());
@@ -112,7 +146,7 @@ public class WikiSearchEngine extends DefaultSearchEngine {
                     wsr.setPageTitle((String) doc.get("titleText"));
 
                     //not so performatic though, better sparql query...will change it later
-                    wsr.setPageURL(JsonReader.getElementValueFromURL("https://" + query.getLanguage() + ".wikipedia.org/w/api.php?action=query&format=json&prop=info&pageids=" + wsr.getPageID().toString() + "&inprop=url",
+                    wsr.setPageURL(JsonReader.getElementValueFromURL("https://" + language + ".wikipedia.org/w/api.php?action=query&format=json&prop=info&pageids=" + wsr.getPageID().toString() + "&inprop=url",
                             "query;pages;" + wsr.getPageID().toString() + ";fullurl"));
                     lc_results.put((String) doc.get("id"), wsr);
 
@@ -183,7 +217,6 @@ public class WikiSearchEngine extends DefaultSearchEngine {
 
                     _id = (Integer) _qs.getLiteral("o").getValue();
 
-
                     WikipediaSearchResult _wsr = lc_results.get(_id.toString());
                     if (_wsr != null){
                         _wsr.setPageURL(_qs.getResource("x").getURI());
@@ -216,6 +249,14 @@ public class WikiSearchEngine extends DefaultSearchEngine {
                 }
 
                 /*****************************************************
+                 implement crawler to find correlated websites from external links
+                 https://github.com/yasserg/crawler4j/blob/master/src/test/java/edu/uci/ics/crawler4j/examples/basic/BasicCrawlController.java
+                 *****************************************************/
+
+
+
+
+                /*****************************************************
                  adding all to websites (wiki and external links)
                  *****************************************************/
                 for (WikipediaSearchResult r : lc_results.values()) {
@@ -223,8 +264,8 @@ public class WikiSearchEngine extends DefaultSearchEngine {
                     WebSite website = new WebSite(query, r.getPageURL());
                     website.setTitle(r.getPageTitle());
                     website.setRank(iaux++);
-                    website.setLanguage(query.getLanguage());
-                    results.add(website);
+                    website.setLanguage(language);
+                    websites.add(website);
 
                     for (String externalLink : r.getExternalLinksfromDBPedia()) {
                         URL aURL = new URL(externalLink);
@@ -232,32 +273,31 @@ public class WikiSearchEngine extends DefaultSearchEngine {
                         WebSite website2 = new WebSite(query, externalLink);
                         website2.setTitle(aURL.getHost());
                         website2.setRank(iaux++);
-                        website2.setLanguage(query.getLanguage());
-                        results.add(website2);
+                        website2.setLanguage(language);
+                        websites.add(website2);
 
                     }
 
                 }
 
-                return new DefaultSearchResult(results, response.getResults().getNumFound(), query, pattern, false);
+                return new DefaultSearchResult(websites, response.getResults().getNumFound(), query, false);
 
             }
             else{
-                return new DefaultSearchResult(new ArrayList<WebSite>(), 0L, query, pattern, false);
+                return new DefaultSearchResult(new ArrayList<WebSite>(), 0L, query, false);
             }
         }
         catch (Exception e) {
 
             e.printStackTrace();
-            return new DefaultSearchResult(new ArrayList<WebSite>(), 0L, query, pattern, false);
+            return new DefaultSearchResult(new ArrayList<WebSite>(), 0L, query, false);
         }
+
     }
 
     @Override
     public String generateQuery(MetaQuery query) {
-
         return new SolrWikiQuery().generateQuery(query);
     }
-
 
 }
