09f25279db6d865d64c35f3010d8acfb9f67a3c5
renespeck <speckrene@gmail.com>
2015-10-02 00:32:16 +0200

    Adds icon.

---
+++ defacto-restful/src/main/java/org/aksw/defacto/restful/webservice/FaviconController.java
@@ -0,0 +1,13 @@
+package org.aksw.defacto.restful.webservice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class FaviconController {
+    @RequestMapping("favicon.ico")
+    @ResponseBody
+    void favicon() {
+    }
+}
