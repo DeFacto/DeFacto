70e7c3e23f189b391a2a3fb7f6ed20645e491c0e
renespeck <speckrene@gmail.com>
2015-10-17 06:39:39 +0200

    Update frontend.

--- defacto-restful/src/main/resources/public/js/controller/facts.js
+++ defacto-restful/src/main/resources/public/js/controller/facts.js
@@ -3,52 +3,49 @@
 angular.module('defacto.controllers.facts', [])
   .controller('FactsCtrl', function($location, $routeParams, $scope, $http, ChartFactory) {
     $scope.requested = 0;
-
-    function fetchdata() {
-      if ($routeParams.name) {
-        // call backend with given id
-        $scope.requested = 1;
-        $http
-          .get('/fusion/id/' + $routeParams.name)
-          .success(function(datas) {
-            console.log(datas);
-            if (datas && typeof datas === 'object' && datas !== null && datas.length) {
-              $scope.facts = {
-                id: $routeParams.name,
-                fact: []
-              };
-              angular.forEach(datas, function(data) {
-                $scope.facts.fact.push(new ChartFactory().initialize(data));
-              });
-            } else {
-              $scope.status = 'No valid data!';
-            }
-          }).error(function(err) {
-            $scope.status = err;
-          });
-      }
+    if ($routeParams.name) {
+      // call backend with given id
+      $scope.requested = 1;
+      $http
+        .get('/fusion/id/' + $routeParams.name)
+        .success(function(triple) {
+          console.log(triple);
+          if (triple) {
+            $scope.triple = triple;
+            $scope.triple.name = $routeParams.name;
+            angular.forEach(triple.data, function(data) {
+              $scope.triple.data.result = [];
+              $scope.triple.data.result.push(new ChartFactory().initialize(data.result));
+            });
+          } else {
+            $scope.status = 'No valid data!';
+          }
+        }).error(function(err) {
+          $scope.status = err;
+        });
     }
-    fetchdata();
+
 
     // voting
-    $scope.upvote = function(fact) {
-      vote(fact, 1);
+    $scope.upvote = function(data) {
+      vote(data, 1);
     };
-    $scope.downvote = function(fact) {
-      vote(fact, -1);
+    $scope.downvote = function(data) {
+      vote(data, -1);
     };
 
-    var vote = function(fact, score) {
+    var vote = function(data, dir) {
       $http
         .post('/fusion/vote/', {
-          factId: $scope.facts.id,
-          votes: score,
-          s: fact.s,
-          p: fact.p,
-          o: fact.o
+          id:data._id.$oid,
+          votes: dir
         })
-        .success(function(data) {
-            fetchdata();
+        .success(function(d) {
+          if(dir>0){
+            data.upvotes++;
+          }else {
+              data.downvotes++;
+          }
         })
         .error(function(err) {
           $scope.status = err;
