51ef0874e5483d4d6c9d4ce8e13c3fa1c1d02eaf
renespeck <speckrene@gmail.com>
2016-03-10 07:14:34 +0100

    Catch config NullPointer.

--- defacto-restful/src/main/java/org/aksw/defacto/restful/webservice/App.java
+++ defacto-restful/src/main/java/org/aksw/defacto/restful/webservice/App.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.management.ManagementFactory;
 
 import org.aksw.defacto.Defacto;
@@ -17,97 +18,99 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 
+ *
  * @author rspeck
  *
  */
 @SpringBootApplication
 public class App {
-    static {
-        PropertyConfigurator.configure(Cfg.LOG_FILE);
+  static {
+    PropertyConfigurator.configure(Cfg.LOG_FILE);
+  }
+
+  public static Logger LOG = LogManager.getLogger(App.class);
+
+  /**
+   *
+   * @param args
+   */
+  public static void main(final String[] args) {
+    try {
+      // write shut down file
+      writeShutDownFile("stop");
+
+      final InputStream stream = Defacto.class.getClassLoader().getResourceAsStream("defacto.ini");
+      if (stream == null) {
+        LOG.error("Could not load resource file \"defacto.ini\"");
+        return;
+      }
+
+      // load config
+      Defacto.DEFACTO_CONFIG = new DefactoConfig(new Ini(stream));
+      // run app
+      SpringApplication.run(App.class, args);
+      // shutdown hook
+      Runtime.getRuntime().addShutdownHook(new Thread((Runnable) () -> {
+        // TODO: check data then shutdown the server
+        //
+        //
+        LOG.info("Stopping server with shutdownHook.");
+      } , "shutdownHook"));
+
+    } catch (final IOException e) {
+      LOG.error(e.getLocalizedMessage(), e);
     }
-    public static Logger LOG = LogManager.getLogger(App.class);
+  }
 
-    /**
-     * 
-     * @param args
-     */
-    public static void main(String[] args) {
+  /**
+   * Gives the applications process id.
+   *
+   * @return applications process id
+   */
+  public static synchronized String getProcessId() {
 
-        writeShutDownFile("stop");
+    final String jvmName = ManagementFactory.getRuntimeMXBean().getName();
+    final int index = jvmName.indexOf('@');
+    if (index < 1) {
+      return null;
+    }
+    try {
+      return Long.toString(Long.parseLong(jvmName.substring(0, index)));
+    } catch (final NumberFormatException e) {
+      return null;
+    }
+  }
 
-        try {
-            Defacto.DEFACTO_CONFIG = new DefactoConfig(new Ini(Defacto.class.getClassLoader().getResourceAsStream("defacto.ini")));
-            // config test, try to get a nullpointer exception
-            Defacto.DEFACTO_CONFIG.getStringSetting("evidence", "WORDNET_DICTIONARY").trim();
-        } catch (IOException e) {
-            LOG.error(e.getLocalizedMessage(), e);
-        }
+  /**
+   * Writes a system depended file to shut down the application with kill cmd and process id.
+   *
+   * @return true if the file was written
+   */
+  public static synchronized boolean writeShutDownFile(final String fileName) {
 
-        SpringApplication.run(App.class, args);
-
-        // shutdown hook
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            @Override
-            public void run() {
-                LOG.info("Checking data before shutting down server...");
-                // TODO: check data then shut down server.
-                //
-                //
-                LOG.info("Stopping server with shutdownHook.");
-
-            }
-        }, "ServerShutdownHook"));
+    // get process Id
+    final String id = getProcessId();
+    if (id == null) {
+      return false;
     }
 
-    /**
-     * Gives the applications process id.
-     * 
-     * @return applications process id
-     */
-    public static synchronized String getProcessId() {
+    String cmd = "";
+    String fileExtension = "";
 
-        final String jvmName = ManagementFactory.getRuntimeMXBean().getName();
-        final int index = jvmName.indexOf('@');
-        if (index < 1)
-            return null;
-        try {
-            return Long.toString(Long.parseLong(jvmName.substring(0, index)));
-        } catch (NumberFormatException e) {
-            return null;
-        }
+    cmd = "kill " + id + System.getProperty("line.separator") + "rm " + fileName + ".sh";
+    fileExtension = "sh";
+    LOG.info(fileName + "." + fileExtension);
+
+    final File file = new File(fileName + "." + fileExtension);
+    try {
+      final BufferedWriter out = new BufferedWriter(new FileWriter(file));
+      out.write(cmd);
+      out.close();
+    } catch (final Exception e) {
+      LOG.error(e.getMessage());
     }
-
-    /**
-     * Writes a system depended file to shut down the application with kill cmd
-     * and process id.
-     * 
-     * @return true if the file was written
-     */
-    public static synchronized boolean writeShutDownFile(String fileName) {
-
-        // get process Id
-        String id = getProcessId();
-        if (id == null)
-            return false;
-
-        String cmd = "";
-        String fileExtension = "";
-
-        cmd = "kill " + id + System.getProperty("line.separator") + "rm " + fileName + ".sh";
-        fileExtension = "sh";
-        LOG.info(fileName + "." + fileExtension);
-
-        File file = new File(fileName + "." + fileExtension);
-        try {
-            BufferedWriter out = new BufferedWriter(new FileWriter(file));
-            out.write(cmd);
-            out.close();
-        } catch (Exception e) {
-            LOG.error(e.getMessage());
-        }
-        file.setExecutable(true, false);
-        file.deleteOnExit();
-        return true;
-    }
+    file.setExecutable(true, false);
+    file.deleteOnExit();
+    return true;
+  }
 }
